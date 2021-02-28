6d5a4fc8680c13f31682aa314d5ee8405ff02ec8
Renee Zieba <Reneezieba@att.net>
2021-02-27 21:54:52 -0600

    Assignment4

--- src/week3/AddCommasToNumberStrings.java
+++ src/week3/AddCommasToNumberStrings.java
@@ -3,9 +3,37 @@ package week3;
 import acm.program.ConsoleProgram;
 
 public class AddCommasToNumberStrings extends ConsoleProgram {
-	
+
 	public void run() {
+		while (true) {
+			String digits = readLine("Enter a numeric string: ");
+			if (digits.length() == 0)
+				break;
+			println(addCommasToNumericStringTest(digits));
+		}
 
 	}
 	
+	private String addCommasToNumericStringTest(String digits) {
+		String num = "";
+		int len = digits.length();
+		if (digits.length() >= 3) {
+			for (int i = 1; i <= digits.length(); i++) {
+				len --;
+				num += digits.charAt(len);
+				
+				if (i % 3 == 0 && i != 0) {
+					num += ",";
+				}	
+			}
+			if (num.charAt(num.length()-1) == ',') {
+				num = num.substring(0, num.length() -1);
+			}
+			
+			return (new StringBuilder(num).reverse().toString());
+		} else {
+			return digits;
+		}
+	}
 }
+	
--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -5,7 +5,12 @@ import acm.program.ConsoleProgram;
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
-
+		println(removeAllOccurrences("This is a test", 't'));
+		println(removeAllOccurrences("Summer is here!", 'e'));
+		println(removeAllOccurrences("---0---", '-'));
+	}
+	public String removeAllOccurrences (String str, char ch) {
+		 return str.replace(String.valueOf(ch), "");
 	}
 	
 }
